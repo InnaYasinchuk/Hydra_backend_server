6382b6a3c1dbb6e083f24fe02833404b47aac24f
Inna Yasynchuk <i_bybyk@ukr.net>
2023-04-28 21:41:33 +0300

    some improvements

--- index.js
+++ index.js
@@ -4,17 +4,20 @@ const nodemailer = require('nodemailer');
 
 const app = express();
 
-app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://hydra-vr-virtual-reality.netlify.app");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors({
+  origin: 'https://hydra-vr-virtual-reality.netlify.app'
+}));
 
 app.post('/send-email', async (req, res) => {
+  res.header("Access-Control-Allow-Origin", "https://hydra-vr-virtual-reality.netlify.app");
+  res.header("Access-Control-Allow-Methods", "POST");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  
+
   const { firstName, lastName, email, phone, subject, about } = req.body;
 
   let transporter = nodemailer.createTransport({
