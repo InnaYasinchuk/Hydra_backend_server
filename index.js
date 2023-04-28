5d58826131c26a6870f1b4a0e04241a6c84189b9
Inna Yasynchuk <i_bybyk@ukr.net>
2023-04-28 16:24:21 +0300

    server settings

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://hydra-vr-virtual-reality.netlify.app");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.post('/send-email', async (req, res) => {
   const { firstName, lastName, email, phone, subject, about } = req.body;
 
